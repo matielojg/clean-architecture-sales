20da212c0110893302ceacd4449a5ed45cc53e10
Matielo J Geronimo <matielojg@gmail.com>
2025-04-11 19:06:51 -0300

    feat(usecase): implement CadastrarRevenda use case

---
+++ core-usecase/src/main/java/com/github/matielojg/revenda/core/usecase/CadastrarRevenda.java
@@ -0,0 +1,7 @@
+package com.github.matielojg.revenda.core.usecase;
+
+import com.github.matielojg.revenda.core.domain.entity.Revenda;
+
+public interface CadastrarRevenda {
+    void executar(Revenda revenda);
+}

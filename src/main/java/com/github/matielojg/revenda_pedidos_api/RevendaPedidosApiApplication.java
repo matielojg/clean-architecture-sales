c97a57cb3bbb10c7d3b7a307099e5d1da3a7d710
Matielo Jose Geronimo <matielo.dev@gmail.com>
2025-04-11 00:13:36 -0300

    feat(init): initialize project structure with Gradle and Spring Boot setup

---
+++ src/main/java/com/github/matielojg/revenda_pedidos_api/RevendaPedidosApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.matielojg.revenda_pedidos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevendaPedidosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevendaPedidosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/matielojg/revenda_pedidos_api/RevendaPedidosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.matielojg.revenda_pedidos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevendaPedidosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

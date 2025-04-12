016fdc86b816767815d488fa18bbd686db35d26c
Matielo J Geronimo <matielojg@gmail.com>
2025-04-12 14:32:38 -0300

    chore(core): update Gradle config for domain module

--- core-domain/build.gradle.kts
+++ core-domain/build.gradle.kts
@@ -2,6 +2,17 @@ plugins {
     java
 }
 
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
 dependencies {
-    
+    // JUnit 5
+    val junitVersion = rootProject.extra["junitVersion"] as String
+    testImplementation("org.junit.jupiter:junit-jupiter:$junitVersion")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }

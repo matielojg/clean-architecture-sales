fa2233408970f374356d6fa0f2b2461e276597fb
Matielo J Geronimo <matielojg@gmail.com>
2025-04-12 14:30:48 -0300

    test(usecase): add unit test for RegisterResellerImpl

--- core-usecase/src/test/java/com/github/matielojg/revenda/core/usecase/impl/RegisterResellerImplTest.java
+++ core-usecase/src/test/java/com/github/matielojg/revenda/core/usecase/impl/RegisterResellerImplTest.java
@@ -3,17 +3,19 @@ package com.github.matielojg.revenda.core.usecase.impl;
 import com.github.matielojg.revenda.core.domain.entity.Reseller;
 import com.github.matielojg.revenda.core.gateway.ResellerRepository;
 import org.junit.jupiter.api.Test;
+
 import static org.mockito.Mockito.*;
 
 class RegisterResellerImplTest {
 
     @Test
     void shouldCallSaveWhenResellerIsValid() {
+        // seguindo o padrao de escrita AAA
         ResellerRepository repository = mock(ResellerRepository.class);
-        RegisterResellerImpl usecase = new RegisterResellerImpl(repository);
-
+        RegisterResellerImpl useCase = new RegisterResellerImpl(repository);
         Reseller reseller = mock(Reseller.class);
-        usecase.execute(reseller);
+
+        useCase.execute(reseller);
 
         verify(repository, times(1)).save(reseller);
     }

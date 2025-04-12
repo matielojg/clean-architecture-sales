6f86e20aa65ed9be8f2fae6154eac6a8943e52d5
Matielo J Geronimo <matielojg@gmail.com>
2025-04-11 22:30:26 -0300

    feat(api): move domain conversion to RegisterResellerRequest

--- api-rest/src/main/java/com/github/matielojg/revenda/api/controller/ResellerController.java
+++ api-rest/src/main/java/com/github/matielojg/revenda/api/controller/ResellerController.java
@@ -1,9 +1,6 @@
 package com.github.matielojg.revenda.api.controller;
 
 import com.github.matielojg.revenda.api.dto.RegisterResellerRequest;
-import com.github.matielojg.revenda.core.domain.entity.Reseller;
-import com.github.matielojg.revenda.core.domain.vo.Cnpj;
-import com.github.matielojg.revenda.core.domain.vo.Email;
 import com.github.matielojg.revenda.core.usecase.RegisterReseller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,8 +8,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/resellers")
@@ -26,15 +21,7 @@ public class ResellerController {
 
     @PostMapping
     public ResponseEntity<Void> register(@RequestBody RegisterResellerRequest request) {
-        Reseller reseller = new Reseller(
-                UUID.randomUUID(),
-                new Cnpj(request.cnpj()),
-                request.name(),
-                new Email(request.email())
-        );
-
-        registerReseller.execute(reseller);
-
+        registerReseller.execute(request.toDomain());
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
--- api-rest/src/main/java/com/github/matielojg/revenda/api/dto/RegisterResellerRequest.java
+++ api-rest/src/main/java/com/github/matielojg/revenda/api/dto/RegisterResellerRequest.java
@@ -1,3 +1,19 @@
 package com.github.matielojg.revenda.api.dto;
 
-public record RegisterResellerRequest(String cnpj, String name, String email) {}
+import com.github.matielojg.revenda.core.domain.entity.Reseller;
+import com.github.matielojg.revenda.core.domain.vo.Cnpj;
+import com.github.matielojg.revenda.core.domain.vo.Email;
+
+import java.util.UUID;
+
+public record RegisterResellerRequest(String cnpj, String name, String email) {
+
+    public Reseller toDomain() {
+        return new Reseller(
+                UUID.randomUUID(),
+                new Cnpj(this.cnpj),
+                this.name,
+                new Email(this.email)
+        );
+    }
+}

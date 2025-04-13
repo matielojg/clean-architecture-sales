0cc181c770009e94f434829c113055e764e5b8da
Matielo J Geronimo <matielojg@gmail.com>
2025-04-13 00:05:39 -0300

    feat(salesorder-adapter-gateway-distributor): add mock controller and distributor config

---
+++ salesorder-adapter-gateway-distributor/src/main/java/com/github/matielojg/salesorder/adapter/gateway/DistributorGatewayConfig.java
@@ -0,0 +1,17 @@
+package com.github.matielojg.salesorder.adapter.gateway;
+
+import com.github.matielojg.salesorder.core.gateway.DistributorGateway;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class DistributorGatewayConfig {
+
+    @Bean
+    public DistributorGateway distributorGateway(RestTemplate restTemplate,
+                                                 @Value("${distributor.url}") String url) {
+        return new DistributorHttpClient(restTemplate, url);
+    }
+}
---
+++ salesorder-api-rest/src/main/java/com/github/matielojg/salesorder/api/controller/MockDistributorController.java
@@ -0,0 +1,21 @@
+package com.github.matielojg.salesorder.api.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/orders")
+public class MockDistributorController {
+
+    private static final Logger log = LoggerFactory.getLogger(MockDistributorController.class);
+
+    @PostMapping
+    public ResponseEntity<Void> receiveOrder(@RequestBody Map<String, Object> body) {
+        log.info("Received order mock: {}", body);
+        return ResponseEntity.ok().build();
+    }
+}

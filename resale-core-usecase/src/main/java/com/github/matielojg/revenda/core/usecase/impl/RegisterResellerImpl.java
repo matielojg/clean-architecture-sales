a039d682b00ddf024b648f244ebb6a799786d137
Matielo J Geronimo <matielojg@gmail.com>
2025-04-13 21:32:20 -0300

    refactor(email): rename Email to EmailAddress to resolve conflict with Email from jakarta

--- resale-core-usecase/src/main/java/com/github/matielojg/revenda/core/usecase/impl/RegisterResellerImpl.java
+++ resale-core-usecase/src/main/java/com/github/matielojg/revenda/core/usecase/impl/RegisterResellerImpl.java
@@ -2,7 +2,7 @@ package com.github.matielojg.revenda.core.usecase.impl;
 
 import com.github.matielojg.revenda.core.domain.entity.Reseller;
 import com.github.matielojg.revenda.core.domain.vo.Cnpj;
-import com.github.matielojg.revenda.core.domain.vo.Email;
+import com.github.matielojg.revenda.core.domain.vo.EmailAddress;
 import com.github.matielojg.revenda.core.gateway.ResellerRepository;
 import com.github.matielojg.revenda.core.usecase.RegisterReseller;
 
@@ -29,7 +29,7 @@ public class RegisterResellerImpl implements RegisterReseller {
                 UUID.randomUUID(),
                 new Cnpj(cnpj),
                 name,
-                new Email(email)
+                new EmailAddress(email)
         );
         execute(reseller);
     }

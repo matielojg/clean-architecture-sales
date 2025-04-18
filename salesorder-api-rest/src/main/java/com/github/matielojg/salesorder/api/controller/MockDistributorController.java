8b4530a5528e64a4bffd53b24e6ef1eefffa23bc
Matielo J Geronimo <matielojg@gmail.com>
2025-04-18 12:21:09 -0300

    feat(mock): add HTTP mock for distributor system

--- salesorder-api-rest/src/main/java/com/github/matielojg/salesorder/api/controller/MockDistributorController.java
+++ salesorder-api-rest/src/main/java/com/github/matielojg/salesorder/api/controller/MockDistributorController.java
@@ -1,5 +1,6 @@
 package com.github.matielojg.salesorder.api.controller;
 
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;

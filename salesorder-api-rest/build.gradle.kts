ce17e1f2faca8683d0f7f0606a579bf89a61a59c
Matielo Jose Geronimo <matielo.dev@gmail.com>
2025-07-24 01:24:46 -0300

    feat: add Dockerfiles and update docker-compose for salesorder and resale APIs

--- salesorder-api-rest/build.gradle.kts
+++ salesorder-api-rest/build.gradle.kts
@@ -51,7 +51,7 @@ tasks.test {
     useJUnitPlatform()
 }
 tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
-    enabled = false
+    enabled = project.hasProperty("enableBootJar")
 }
 
 tasks.named<Jar>("jar") {
